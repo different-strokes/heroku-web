722095474737c4bbb57f6a3e011b0e77768443b6
Fredrik Stave <fredrik.stave@e24.no>
2018-09-20 20:29:33 +0200

    feat: added Koa server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello World!';
+});
+
+router.get('/status', async (ctx, next) => {
+  ctx.body = 'OK';
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
